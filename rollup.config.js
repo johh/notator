0aff03261c4e35c10aed71768717427652c099bf
JohH <code@johh.net>
2018-07-08 16:51:14 +0200

    FEATURE: build umd package using rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/index.js',
+		format: 'umd',
+		name: 'notator',
+	},
+	plugins: [
+		resolve(),
+		babel({
+			exclude: 'node_modules/**', // only transpile our source code
+		}),
+		uglify(),
+	],
+};
