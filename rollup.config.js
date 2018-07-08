a941899e42c65b3577c8d84195e295d08bf9e655
JohH <code@johh.net>
2018-07-08 17:30:14 +0200

    TASK: changed licence to BSD-4-Clause

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 
+import info from './package.json';
+
 
 export default {
 	input: 'src/index.js',
@@ -9,12 +11,22 @@ export default {
 		file: 'dist/index.js',
 		format: 'umd',
 		name: 'notator',
+		banner: `/**
+* NOTATOR v${info.version}
+* © 2018 JohH <code@johh.net>
+* licensed under BSD-4-Clause
+* https://johh.net/notator
+**/`,
 	},
 	plugins: [
 		resolve(),
 		babel({
 			exclude: 'node_modules/**', // only transpile our source code
 		}),
-		uglify(),
+		uglify({
+			output: {
+				comments: /^\*/,
+			},
+		}),
 	],
 };
